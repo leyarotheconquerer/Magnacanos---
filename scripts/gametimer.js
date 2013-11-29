e28725af97bd9233689c140e7da168dd5d712f56
dreac0nic <spenser.m.bray@gmail.com>
2013-11-28 21:04:23 -0600

    Added code for basic game timer.

---
+++ scripts/gametimer.js
@@ -0,0 +1,28 @@
+// Timer class for proper milliseconds timing.
+function GameTimer()
+{
+    this.firstMillis = 0;
+    this.averageDuration = 0;
+    this.duration = 0
+    
+    // Resets and starts the timer.
+    this.start = function() {
+	var date = new Date();
+	
+	this.firstMillis = date.getTime();
+	this.duration = 0;
+    }
+    
+    // Ends the timer, setting the duration and averaging it with the last durations.
+    this.end = function() {
+	var date = new Date();
+	
+	currentMillis = date.getTime();
+	this.duration = this.firstMillis - currentMillis;
+    }
+    
+    // Returns the desired framedelay for the next frame based on the framerate.
+    this.getDelayToNextFrame = function(desiredFramerate) {
+	return 1/desiredFramerate - this.lastDuration;
+    }
+}
