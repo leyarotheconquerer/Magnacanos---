340e2c181e6ddde5c9d83b8a9925fc29eaba47e2
drac0nic <spenser.m.bray@gmail.com>
2014-02-22 10:32:28 -0600

    Switch engine to setInterval based update.

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,27 @@ var engine;
 
 window.onload = function()
 {
-	// Declare variables ...
-	var timer = new GameTimer();
-	
-	engine = new Engine();
-	
-	var canvas;
-	var context;
-	
-	var width = 800;
-	var height = 600;
-	
-	// Initialize canvas and context ...
-	canvas = document.getElementById("canvas");
-	
-	if(canvas != null) {
-		if(!(context = canvas.getContext("2d"))) {
-			alert("Could not load canvas context.");
-		}
+    // Declare variables ...
+    var timer = new GameTimer();
+    
+    engine = new Engine();
+    
+    var canvas;
+    var context;
+    
+    var width = 800;
+    var height = 600;
+    
+    // Initialize canvas and context ...
+    canvas = document.getElementById("canvas");
+    
+    if(canvas != null) {
+	if(!(context = canvas.getContext("2d"))) {
+	    alert("Could not load canvas context.");
 	}
-	
-	engine.init(context);
-	
-        engine.run();
+    }
+    
+    engine.init(context);
+    
+    window.setInterval(engine.run, 1000);
 };
