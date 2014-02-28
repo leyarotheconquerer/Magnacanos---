ecff2820991b47015d413bd4ad107ac0421f27af
drac0nic <spenser.m.bray@gmail.com>
2014-02-28 11:35:25 -0600

    Tune timer to not yeild values equal to or less then zero.

--- scripts/gametimer.js
+++ scripts/gametimer.js
@@ -1,38 +1,42 @@
 // Timer class for proper milliseconds timing.
 GameTimer = function()
 {
-	this.firstMillis = 0;
-	this.averageDuration = 0;
-	this.duration = 0;
+    var that = this; // Create a handle to the instance of this entity.
+
+    this.firstMillis = 0;
+    this.averageDuration = 0;
+    this.duration = 0;
+    
+    // Resets and starts the timer.
+    this.start = function() {
+	var date = new Date();
+        
+	that.firstMillis = date.getTime();
+	that.duration = 0;
+    };
+    
+    // Ends the timer, setting the duration and averaging it with the last durations.
+    this.end = function() {
+	var date = new Date();
 	
-	// Resets and starts the timer.
-	this.start = function() {
-		var date = new Date();
-                
-		this.firstMillis = date.getTime();
-		this.duration = 0;
-	};
+	currentMillis = date.getTime();
+	that.duration = currentMillis - that.firstMillis;
 	
-	// Ends the timer, setting the duration and averaging it with the last durations.
-	this.end = function() {
-		var date = new Date();
-		
-		currentMillis = date.getTime();
-		this.duration = currentMillis - this.firstMillis;
-		
-		// A temporary implementation of averaging duration, really it calculates some mix between latest duration and average
-		if(this.averageDuration == 0)
-		{
-			this.averageDuration = this.duration;
-		}
-		else
-		{
-			this.averageDuration = (this.averageDuration + this.duration) / 2.0;
-		};
+	// A temporary implementation of averaging duration, really it calculates some mix between latest duration and average
+	if(that.averageDuration == 0)
+	{
+	    that.averageDuration = that.duration;
+	}
+	else
+	{
+	    that.averageDuration = (that.averageDuration + that.duration) / 2.0;
 	};
+    };
+    
+    // Returns the desired framedelay for the next frame based on the framerate.
+    this.getDelayToNextFrame = function(desiredFramerate) {
+	var calculatedTime = ((1.0/desiredFramerate*1000) - that.duration);
 	
-	// Returns the desired framedelay for the next frame based on the framerate.
-	this.getDelayToNextFrame = function(desiredFramerate) {
-		return ((1.0/desiredFramerate*1000) - this.duration);
-	};
+	return (calculatedTime > 0 ? calculatedTime : 1);
+    };
 };
