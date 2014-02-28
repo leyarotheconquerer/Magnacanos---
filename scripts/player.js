6e08253bdb13803251efeae78ee4cc72a5043a50
drac0nic <spenser.m.bray@gmail.com>
2014-02-28 12:38:24 -0600

    `Implement` Player class..........

--- scripts/player.js
+++ scripts/player.js
@@ -1,4 +1,11 @@
 Player = function()
 {
     var that = this; // Fixing Javascripts scoping issues for 'this' references. I am pretty sure this comment changes everytime I write it.
+    
+    // MEMBER VARIABLES --
+    this.x = 0;
+    this.y = 0;
+    
+    // FUNCTIONS --
+    // WHO NEEDS FUNCTIONS SCREW FUNCTIONS BLARG
 }
