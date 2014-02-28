47f370c38bde6f2244e0a18617078fdf00682fc0
drac0nic <spenser.m.bray@gmail.com>
2014-02-28 12:45:04 -0600

    Implement update and draw in player.

--- scripts/player.js
+++ scripts/player.js
@@ -8,4 +8,22 @@ Player = function()
     
     // FUNCTIONS --
     // WHO NEEDS FUNCTIONS SCREW FUNCTIONS BLARG
+    // Player update function, because palyers do taht yeah.
+    this.update = function()
+    {
+	// Update player?
+
+	// NOTE: Player is not a physics object, so yeah. I guess he could be but that could be deadly.
+	//         Let's jsut make him run around isntead.
+	
+	// Run around?
+    }
+
+    this.draw = function(context)
+    {
+	context.save();
+	context.translate(that.x, that.y);
+	context.rotate(that.rotation);
+	context.drawImage("./assets/images/dashie.png"); // TEMP IMAGE
+	context.restore();
 }
